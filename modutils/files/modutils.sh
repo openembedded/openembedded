3b3989442075d0a4c6c32cb187de17d87bf65fbd
Holger Schurig <schurig@mn-solutions.de>
2004-09-21 11:18:50 +0000

    made it source-able removed the modutils -t boot stuff, -t is not supported by busybox modutils anyway added a streamlined modutils.sh to MNCI "Ramses" for faster boot
    BKrev: 41500e1amZdvSJObOllaQ_D6QtMKdQ

---
+++ modutils/files/modutils.sh
@@ -0,0 +1,17 @@
+[ -f /proc/modules ] || exit 0
+[ -e /sbin/depmod ] || exit 0
+
+[ "$VERBOSE" != no ] && echo "Calculating module dependencies ..."
+depmod -Ae
+
+[ "$VERBOSE" != no ] && echo -n "Loading modules: "
+(cat /etc/modules; echo; ) |
+while read module args
+do
+	case "$module" in
+		\#*|"") continue ;;
+	esac
+	[ "$VERBOSE" != no ] && echo -n "$module "
+	modprobe $module $args
+done
+[ "$VERBOSE" != no ] && echo
