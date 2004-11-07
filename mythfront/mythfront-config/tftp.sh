4778a79d213aa1b1f09f6be51971ddb285c3dd81
Phil Blundell <philb@gnu.org>
2004-11-07 14:53:31 +0000

    add mythfront.config support
    BKrev: 418e36ebE19m1KBwQdeIF0SIN9RBxA

--- mythfront/mythfront-config/tftp.sh
+++ mythfront/mythfront-config/tftp.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+case $1 in
+  renew|bound)
+  	;;
+  *)
+  	exit 0
+	;;
+esac
+
 bootdir=`dirname $boot_file`
 
-files="mysql.txt xorg.conf"
+files="mysql.txt lircrc mythfront.config"
 
 mkdir /var/lib/config
 cd /var/lib/config
@@ -14,3 +22,15 @@ for fn in $files; do
     fi
   fi
 done
+
+if [ -f ./mythfront.config ]; then
+  . ./mythfront.config
+  if [ "x$REMOTE" != "x" ]; then
+    fn=`find /usr/share/lirc/remotes -name lircd.conf.$REMOTE`
+    if [ "x$fn" != "x" ]; then
+      rm -f lircd.conf
+      ln -sf $fn lircd.conf
+    fi
+  fi
+fi
+
