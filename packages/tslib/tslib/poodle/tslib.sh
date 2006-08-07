c3891ecfcc733c7bdaf047a680ddc42895699b68
Marcin Juszkiewicz <hrw@openembedded.org>
2006-08-07 23:23:37 +0000

    tslib: finally resolved poodle 2.4/2.6 selection

--- packages/tslib/tslib/poodle/tslib.sh
+++ packages/tslib/tslib/poodle/tslib.sh
@@ -3,10 +3,10 @@
 if (uname -r|grep -q 'embedix'); then
 	TSLIB_TSDEVICE=/dev/ts
 	TSLIB_TSEVENTTYPE=COLLIE
+	TSLIB_CONFFILE=/usr/share/tslib/ts-2.4.conf
 else
 	TSLIB_TSDEVICE=/dev/input/event1
+	TSLIB_CONFFILE=/usr/share/tslib/ts-2.6.conf
 fi
-
-TSLIB_CONFFILE=/usr/share/tslib/ts.conf
 
 export TSLIB_TSDEVICE TSLIB_TSEVENTTYPE TSLIB_CONFFILE
