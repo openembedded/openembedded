6e93602d9bb4ffe35c5d7a2a7870b89c1e4fb630
Paul Sokolovsky <pmiscml@gmail.com>
2006-08-22 12:21:28 +0000

    tslib: added h4000 support - close #1351

---
+++ packages/tslib/tslib/h4000/tslib.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+module_id() {
+    awk 'BEGIN { FS=": " } /Hardware/ { print $2 } ' </proc/cpuinfo
+}
+
+case `uname -r` in
+2.4*)
+	TSLIB_TSDEVICE=/dev/touchscreen/0raw
+	TSLIB_CONFFILE=/usr/share/tslib/ts.conf-h3600-2.4
+	;;
+*)
+	TSLIB_TSDEVICE=`detect-stylus --device`
+	TSLIB_CONFFILE=/usr/share/tslib/ts-2.6.conf
+	;;
+esac
+
+export TSLIB_TSDEVICE TSLIB_CONFFILE
+
