51497027c61550d72a994ad3161e45f66e8a81bd
Marcin Juszkiewicz <hrw@openembedded.org>
2006-08-29 13:42:00 +0000

    feed-browser: feeds has a type now: normal/upgrades. Type is used by updater to update only 'upgrades' feeds: "php update.php upgrades"

--- contrib/feed-browser/update.php
+++ contrib/feed-browser/update.php
@@ -39,10 +39,15 @@ require_once 'includes/functions.inc';
 
 check_database();
 
+$feeds = db_query("SELECT f_id, f_name, f_uri FROM feeds");
+
+if($argc > 1 AND $argv[1] == 'upgrades')
+{
+	$feeds = db_query("SELECT f_id, f_name, f_uri FROM feeds WHERE f_type = 'upgrades'");
+}
+
 $start = time();
 $p_count = 0;
-
-$feeds = db_query("SELECT f_id, f_name, f_uri FROM feeds");
 
 foreach($feeds as $feed)
 {
