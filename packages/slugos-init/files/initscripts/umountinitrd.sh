ae3e49711d031281251d778d66e96624c492e356
Rod Whitby <rod@whitby.id.au>
2006-11-27 04:32:02 +0000

    slugos-init: Now loades IDE kernel modules for nas100d

--- packages/slugos-init/files/initscripts/umountinitrd.sh
+++ packages/slugos-init/files/initscripts/umountinitrd.sh
@@ -5,20 +5,27 @@
 # then the /initrd is mounted and we want to remount that
 # ro - this works round the shutdown -r hang problem
 . /etc/default/functions
+
+case "$(machine)" in
+nslu2)
+	ffspart="Flashdisk";;
+*)
+	ffspart="filesystem";;
+esac
+
 while read device directory remainder
 do
 	case "$directory" in
 	/mnt)	echo "InitRD: unmount initrd on /mnt" >&2
 		umount /mnt;;
 	/initrd)# need the device for a remount
-		ffspart=Flashdisk
 		ffsdev="$(mtblockdev $ffspart)"
-		echo "InitRD: remount $ffdev read-only on /initrd" >&2
+		echo "Remounting $ffsdev read-only on /initrd" >&2
 		if test -n "$ffsdev" -a -b "$ffsdev"
 		then
 			mount -o remount,ro "$ffsdev" /initrd
 		else
-			echo "Flashdisk: $ffsdev: flash device not found" >&2
+			echo "$ffspart: $ffsdev: flash device not found" >&2
 		fi;;
 	esac
 done </proc/mounts
--- packages/slugos-init/slugos-init_0.10.bb
+++ packages/slugos-init/slugos-init_0.10.bb
@@ -4,7 +4,7 @@ PRIORITY = "required"
 LICENSE = "GPL"
 DEPENDS = "base-files devio"
 RDEPENDS = "busybox devio"
-PR = "r73"
+PR = "r74"
 
 SRC_URI = "file://boot/flash \
 	   file://boot/disk \
