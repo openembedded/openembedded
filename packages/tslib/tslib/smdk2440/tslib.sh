7b614087202bd27111d85e507f21f8ed0a32b890
Graeme Gregory <dp@xora.org.uk>
2006-11-29 10:53:02 +0000

    tslib/tslib/smdk2440/tslib.sh : remove 2.4 stuff that gives away where I stole the file from.

--- packages/tslib/tslib/smdk2440/tslib.sh
+++ packages/tslib/tslib/smdk2440/tslib.sh
@@ -3,11 +3,4 @@
 TSLIB_TSDEVICE=/dev/input/touchscreen0
 TSLIB_CONFFILE=/etc/ts.conf
 
-case `uname -r` in
-2.4*)
-	TSLIB_TSDEVICE=/dev/ts
-	TSLIB_CONFFILE=/usr/share/tslib/ts.conf-corgi-2.4
-	;;
-esac
-
 export TSLIB_TSDEVICE TSLIB_CONFFILE
