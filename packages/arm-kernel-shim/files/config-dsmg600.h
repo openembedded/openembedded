1c318bb5d67b8c0ad2c9d6855f6171c28b4b4e1c
Rod Whitby <rod@whitby.id.au>
2007-02-14 06:16:54 +0000

    arm-kernel-shim: Added PASSTHROUGH_ATAGS support.

--- packages/arm-kernel-shim/files/config-dsmg600.h
+++ packages/arm-kernel-shim/files/config-dsmg600.h
@@ -16,16 +16,19 @@
 
 #define PHYS_PARAMS	   0x00000100 /* Address for the parameter list */
 
-#define RAM_BANK0_START	   0x00000000
-#define RAM_BANK0_LENGTH   0x04000000
+//#define RAM_BANK0_START	   0x00000000
+//#define RAM_BANK0_LENGTH   0x04000000
 
 //#define RAM_BANK1_START	   0xd0000000
 //#define RAM_BANK1_LENGTH   0x10000000
 
-#define COMMANDLINE\
- "init=/linuxrc root=/dev/mtdblock2 rootfstype=jffs2 rw noirqdebug console=ttyS0,115200n8"
+//#define COMMANDLINE\
+// "init=/linuxrc root=/dev/mtdblock2 rootfstype=jffs2 rw noirqdebug console=ttyS0,115200n8"
 
 #define MACH_TYPE		   964
+
+/* The DSM-G600 RedBoot sets up the ATAGs correctly */
+#define PASSTHROUGH_ATAGS
 
 /* Uncomment one of these if you need to switch the CPU into a
    specific mode. */
