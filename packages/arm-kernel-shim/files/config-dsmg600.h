8540f051df91ed228bc49606d7f09d01df939570
Rod Whitby <rod@whitby.id.au>
2007-02-14 05:56:08 +0000

    arm-kernel-shim: first attempt at dmsg600 config

--- packages/arm-kernel-shim/files/config-dsmg600.h
+++ packages/arm-kernel-shim/files/config-dsmg600.h
@@ -22,8 +22,8 @@
 //#define RAM_BANK1_START	   0xd0000000
 //#define RAM_BANK1_LENGTH   0x10000000
 
-//#define COMMANDLINE\
-// "console=ttyAMA0 root=/dev/memblk0 rootfstype=jffs"
+#define COMMANDLINE\
+ "init=/linuxrc root=/dev/mtdblock2 rootfstype=jffs2 rw noirqdebug console=ttyS0,115200n8"
 
 #define MACH_TYPE		   964
 
