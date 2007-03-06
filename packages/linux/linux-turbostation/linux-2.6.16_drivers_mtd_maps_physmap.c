b264d9f686955932910aade9d1cbc67e2db091b6
Oyvind Repvik <nail@nslu2-linux.org>
2007-03-06 22:51:26 +0000

    linux-turbostation: Update flash partitioning

--- packages/linux/linux-turbostation/linux-2.6.16_drivers_mtd_maps_physmap.c
+++ packages/linux/linux-turbostation/linux-2.6.16_drivers_mtd_maps_physmap.c
@@ -1,47 +1,37 @@
-Index: linux-2.6.20.1/drivers/mtd/maps/physmap.c
+Index: linux-2.6.21-rc2-git3/drivers/mtd/maps/physmap.c
 ===================================================================
---- linux-2.6.20.1.orig/drivers/mtd/maps/physmap.c	2007-02-20 07:34:32.000000000 +0100
-+++ linux-2.6.20.1/drivers/mtd/maps/physmap.c	2007-02-26 02:05:28.000000000 +0100
-@@ -134,13 +134,53 @@
+--- linux-2.6.21-rc2-git3.orig/drivers/mtd/maps/physmap.c	2007-03-06 01:06:56.000000000 +0100
++++ linux-2.6.21-rc2-git3/drivers/mtd/maps/physmap.c	2007-03-06 01:12:35.000000000 +0100
+@@ -134,13 +134,43 @@
  	}
  	info->mtd->owner = THIS_MODULE;
  
 +static struct mtd_partition TS101_partitions[] = {
 +	{
-+	  .name = "U-Boot",
++	  .name = "u-boot",
 +	  .offset = 0x00F00000,
 +	  .size = 0x00040000,
 +	  .mask_flags = MTD_WRITEABLE,  /* force read-only */
 +	},
 +	{
-+	  .name = "Kernel",             /* default kernel image */
++	  .name = "kernel",             /* default kernel image */
 +	  .offset = 0x00000000,
-+	  .size = 0x00200000,
++	  .size = 0x00280000,
 +	},
 +	{
-+	  .name = "RootFS1",
-+	  .offset = 0x00200000,
-+	  .size = 0x00900000,
++	  .name = "rootfs",
++	  .offset = 0x00280000,
++	  .size = 0x00C80000,
 +	},
 +	{
-+	  .name = "RootFS2",
-+	  .offset = 0x00b00000,
-+	  .size = 0x00300000,
-+	},
-+	{
-+	  .name = "Vendor",
-+	  .offset = 0x00E00000,
-+	  .size = 0x00100000,
-+	},
-+	{
-+	  .name = "U-Boot Config",
++	  .name = "empty",
 +	  .offset = 0x00F40000,
-+	  .size = 0x00020000,
++	  .size = 0x000A0000,
 +	},
 +	{
-+	  .name = "NAS Config",
-+	  .offset = 0x00F60000,
-+	  .size = 0x000A0000,
++	  .name = "u-boot env",
++	  .offset = 0x00FE0000,
++	  .size = 0x00020000,
 +	}
 +};
 +
@@ -53,7 +43,7 @@ Index: linux-2.6.20.1/drivers/mtd/maps/physmap.c
  		return 0;
  	}
 -
-+	physmap_set_partitions(TS101_partitions, 7);
++	physmap_set_partitions(TS101_partitions, 5);
  	if (physmap_data->nr_parts) {
  		printk(KERN_NOTICE "Using physmap partition information\n");
  		add_mtd_partitions(info->mtd, physmap_data->parts,
