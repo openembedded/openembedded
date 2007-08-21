694a0e949169df619b5f042d86ed640aef3da6c0
Stefan Schmidt <stefan@datenfreihafen.org>
2007-08-21 20:49:27 +0000

    initscripts: Fix script name in save-rtc.sh (Closes OpenMoko bug #725)

--- packages/initscripts/initscripts-1.0/save-rtc.sh
+++ packages/initscripts/initscripts-1.0/save-rtc.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-#FIXME readd timestamp handling for systems where RTC doesn't survive a reboot
-/etc/init.d/hwclock stop
+#FIXME read timestamp handling for systems where RTC doesn't survive a reboot
+/etc/init.d/hwclock.sh stop
