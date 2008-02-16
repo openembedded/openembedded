82e725505a8924059c5c75ed32cf3101ef318c10
Paul Sokolovsky <pmiscml@gmail.com>
2008-02-16 00:17:13 +0000

    initramfs-module-kexecboot: Initramfs module to kexec to kernel in rootfs. * Instead of switch_root'ing to it, which was the only working mode previously. So, now, if there's /boot/zImage in rootfs, it will be kexec'ed too. Otherwise, switch_root will happen. * Caveat: there should be initramfs for new (kexec'ed to) kernel too, too handle its loading of rootfs as selected by interactive bootloader. This can be very same kernel as for bootloader, with bundled initramfs-bootmenu-image (which has special check - if root is explicitly specified on command line, then skip interactive selection); or, as a kind of optimization, that could be initramfs-image (no interactive part). Finally, initramfs (any of there two or other) can be a standalone file in rootfs, /boot/initramfs.bin.

---
+++ packages/initrdscripts/files/87-kexecboot.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Allow kexecing to kernel in rootfs
+
+if [ -n "$BOOT_ROOT" -a -f "$BOOT_ROOT/boot/zImage" ]; then
+    echo "Kernel found in rootfs:"
+    ls -l "$BOOT_ROOT/boot/zImage"
+    initramfs=""
+    if [ -f "$BOOT_ROOT/boot/initramfs.bin" ]; then
+	echo "Initramfs found in rootfs:"
+	ls -l "$BOOT_ROOT/boot/initramfs.bin"
+	initramfs="--initrd=$BOOT_ROOT/boot/initramfs.bin"
+    fi
+    echo /usr/sbin/kexec -f "$BOOT_ROOT/boot/zImage" $initramfs --command-line="$CMDLINE"
+    sleep 10
+    /usr/sbin/kexec -f "$BOOT_ROOT/boot/zImage" $initramfs --command-line="$CMDLINE"
+    sleep 10000
+fi
