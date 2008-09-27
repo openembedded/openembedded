aca5d88e570d195830c2610d9d23c9adca5b6d07
Junqian Gordon Xu <xjqian@gmail.com>
2008-09-27 22:17:28 +0000

    angstrom/build-feeds.sh: add task-gpe, leafpad, x11vnc, fakeroot, gzip

--- contrib/angstrom/build-feeds.sh
+++ contrib/angstrom/build-feeds.sh
@@ -45,7 +45,7 @@ do
         BUILD_TARGETS="texinfo flex bison gperf gcc binutils make automake autoconf m4 pkgconfig distcc \
                        man \
 		       usbutils pciutils mtd-utils usbview hal setserial \
-                       task-proper-tools mc screen findutils \
+                       task-proper-tools mc screen findutils fakeroot gzip \
 		       mono perl \
 		       python python-pygtk \
 		       ruby \
@@ -54,7 +54,7 @@ do
 		       gpe-mini-browser midori minimo openmoko-browser2 webkit-gtklauncher \
 		       samba meta-nas-server \
 		       apache2 boa cherokee lighttpd thttpd \
-		       rox-filer gpe-gallery gpe-scap notecase \
+		       rox-filer gpe-gallery gpe-scap notecase leafpad \
 		       pidgin irssi \
 		       roadmap-gtk2 gpsdrive navit gpsbabel \
 		       xmms mplayer quasar vlc-gpe gnome-mplayer \
@@ -70,12 +70,13 @@ do
 		       asterisk \
 		       gnuradio gnuplot mpfr gmp fftw fftwf fftwl \
 		       gphoto2 gqview imagemagick ufraw \
-		       rdesktop \
+		       rdesktop x11vnc \
 		       tzdata \
 		       xserver-kdrive xserver-xorg \
 		       xf86-video-fbdev xf86-video-ati xf86-video-vesa \
 		       xf86-input-evdev xf86-input-keyboard xf86-input-mouse \
 		       task-opie \
+                       task-gpe-apps task-gpe-pim task-gpe-settings task-gpe-connectivity \
 		       task-openmoko-base task-openmoko-debug task-openmoko-examples task-openmoko-feed task-openmoko-games task-openmoko-linux task-openmoko-native-sdk task-openmoko-net task-openmoko-phone task-openmoko-pim task-openmoko-ui \
 		       hildon-1 hildon-base-lib hildon-lgpl hildon-libs hildon-thumbnail libconic libgpsbt libgpsmgr libhildonfm libhildonhelp libhildonmime libosso-gsf libosso-help libosso mce-dev osso-gwconnect osso-ic-oss osso-thumbnail outo \
 		       kbdd \
