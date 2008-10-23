1f1e1431aa2c97c3d49eac5cc95b439002ec2c87
Michael Lauer <mickeyl@shell.apm.etc.tu-bs.de>
2008-10-23 15:10:19 +0000

    python 2.5.2 limit readline history to 1000

--- packages/python/python-2.5.2/sitecustomize.py
+++ packages/python/python-2.5.2/sitecustomize.py
@@ -1,6 +1,6 @@
 # OpenEmbedded sitecustomize.py (C) 2002-2008 Michael 'Mickey' Lauer <mlauer@vanille-media.de>
 # GPLv2 or later
-# Version: 20082201
+# Version: 20081123
 # Features:
 # * set proper default encoding
 # * enable readline completion in the interactive interpreter
@@ -20,7 +20,8 @@ def __registerExitHandler():
     atexit.register( __exithandler )
 
 def __enableReadlineSupport():
-    readline.parse_and_bind("tab: complete")
+    readline.set_history_length( 1000 )
+    readline.parse_and_bind( "tab: complete" )
     try:
         readline.read_history_file( "%s/.python-history" % os.getenv( "HOME", "/tmp" ) )
     except IOError:
@@ -29,7 +30,7 @@ def __enableReadlineSupport():
 def __enableDefaultEncoding():
     import sys
     try:
-        sys.setdefaultencoding('utf8')
+        sys.setdefaultencoding( "utf8" )
     except LookupError:
         pass
 
