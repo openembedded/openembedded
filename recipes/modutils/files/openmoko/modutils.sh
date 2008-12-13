ec3e5c13ad4dfc411b18c5262d0b09080291e3b4
Klaus Kurzmann <mok@fluxnetz.de>
2008-12-13 10:39:59 +0100

    modutils: Do not run depmod.
    Author: Chia-I Wu <olv@openmoko.com>
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

---
+++ recipes/modutils/files/openmoko/modutils.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+LOAD_MODULE=modprobe
+[ -e /sbin/modprobe ] || LOAD_MODULE=insmod
+
+if [ -f /proc/modules ]; then
+	if [ -f /etc/modules ]; then
+		[ "$VERBOSE" != no ] && echo -n "Loading modules: "
+		while read module args
+		do
+			case "$module" in
+				\#*|"") continue ;;
+			esac
+			[ "$VERBOSE" != no ] && echo -n "$module "
+			eval "$LOAD_MODULE $module $args >/dev/null 2>&1"
+		done < /etc/modules
+		[ "$VERBOSE" != no ] && echo
+	fi
+fi
+
+: exit 0
