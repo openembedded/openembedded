40107a8c5af8d058f18dbe9845cdc47ee46a614e
acid-burn <acidburn@opendreambox.org>
2009-02-13 10:02:40 +0100

    properly unmount network filesystems on shutdown

--- packages/initscripts/initscripts-1.0/umountnfs.sh
+++ packages/initscripts/initscripts-1.0/umountnfs.sh
@@ -21,5 +21,34 @@ do
 done
 ) < /etc/fstab
 
+echo "Unmounting other remote filesystems..."
+
+#
+# Read the list of mounted file systems and -f umount the
+# known network file systems.  -f says umount it even if
+# the server is unreachable.  Do not attempt to umount
+# the root file system.  Unmount in reverse order from
+# that given by /proc/mounts (otherwise it may not work).
+#
+unmount() {
+	local dev mp type opts
+	if read dev mp type opts
+	then
+		# recurse - unmount later items
+		unmount
+		# skip /, /proc and /dev
+		case "$mp" in
+		/|/proc)return 0;;
+		/dev)	return 0;;
+		esac
+		# then unmount this, if nfs
+		case "$type" in
+		nfs|smbfs|ncpfs|cifs) umount -f "$mp";;
+		esac
+	fi
+}
+
+unmount </proc/mounts
+
 : exit 0
 
