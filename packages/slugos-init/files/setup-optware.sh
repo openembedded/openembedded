e198c4d32abf6187d730d3bffc8a87abdfa395f5
Mike Westerhof <mwester@dls.net>
2009-02-13 21:46:28 -0600

    SlugOS: setup-optware.sh - whitespace fix.

--- packages/slugos-init/files/setup-optware.sh
+++ packages/slugos-init/files/setup-optware.sh
@@ -4,7 +4,7 @@ if test 0 != `id -u`; then
     echo 'Please run as root'
     exit 1
 fi
- 
+
 optware_target=slugos5be
 feed=http://ipkg.nslu2-linux.org/feeds/optware/${optware_target}/cross/unstable
 latest_xsh=`wget -q -O- ${feed} | grep '\.xsh' | sed -e 's/.*xsh">//' -e 's/<.*//'`
