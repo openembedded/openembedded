ec8d0041da288ecee9f3e9a85a32661641eaa2d0
Koen Kooi <koen@openembedded.org>
2009-03-31 10:40:15 +0000

    initscripts: set alignment to 0 for armv7a
    Signed-off-by: Koen Kooi <koen@openembedded.org>
    Acked-by: Philip Balister (philip@balister.org)

--- recipes/initscripts/files/arm/alignment.sh
+++ recipes/initscripts/files/arm/alignment.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-if [ -e /proc/cpu/alignment ] && ! [ $(uname -m) = "armv7l" ]; then
-   echo "3" > /proc/cpu/alignment
+if [ -e /proc/cpu/alignment ] ; then
+   if [ $(uname -m) = "armv7l" ]; then
+       echo "0" > /proc/cpu/alignment
+   else
+       echo "3" > /proc/cpu/alignment
+   fi
 fi
 
