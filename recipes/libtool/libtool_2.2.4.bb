openembedded/openembedded

commit 2e0fa4918d643ce6ec0299cd31cfe1c346e4bce3

libtool: Fixed paths to grep and sed.

recipes/libtool/libtool_2.2.4.bb
@@ -1,5 +1,5 @@
 require libtool.inc
-PR = "r15"
+PR = "r16"
 
 SRC_URI = "${GNU_MIRROR}/libtool/libtool-${PV}.tar.gz"
 S = "${WORKDIR}/libtool-${PV}"
@@ -13,6 +13,16 @@ FILES_libltdl-dbg = "${libdir}/.debug/"
 inherit autotools
 
 EXTRA_AUTORECONF = "--exclude=libtoolize"
+
+do_configure_prepend () {
+	# Skip this for native build:
+	if test -n "$CONFIG_SITE" ; then
+		export ac_cv_path_SED=${ac_cv_path_GREP=/bin/sed}
+		export ac_cv_path_GREP=${ac_cv_path_GREP=/bin/grep}
+		export ac_cv_path_EGREP=${ac_cv_path_EGREP=/bin/grep -E}
+		export ac_cv_path_FGREP=${ac_cv_path_FGREP=/bin/grep -F}
+	fi
+}
 
 do_stage () {
        install -d ${STAGING_INCDIR}/libltdl

recipes/libtool/libtool_2.2.6a.bb
@@ -1,5 +1,5 @@
 require libtool.inc
-PR = "r0"
+PR = "r1"
 
 SRC_URI = "${GNU_MIRROR}/libtool/libtool-${PV}.tar.gz"
 S = "${WORKDIR}/${BPN}-2.2.6"
@@ -13,6 +13,16 @@ FILES_libltdl-dbg = "${libdir}/.debug/"
 inherit autotools
 
 EXTRA_AUTORECONF = "--exclude=libtoolize"
+
+do_configure_prepend () {
+	# Skip this for native build:
+	if test -n "$CONFIG_SITE" ; then
+		export ac_cv_path_SED=${ac_cv_path_GREP=/bin/sed}
+		export ac_cv_path_GREP=${ac_cv_path_GREP=/bin/grep}
+		export ac_cv_path_EGREP=${ac_cv_path_EGREP=/bin/grep -E}
+		export ac_cv_path_FGREP=${ac_cv_path_FGREP=/bin/grep -F}
+	fi
+}
 
 do_stage () {
        install -d ${STAGING_INCDIR}/libltdl

commit f88d3e68e399069ba38f21cd4aa02b9dfcf487fc

libtool: 2e0fa4918d643ce6ec0299cd31cfe1c346e4bce3 broke do_configure. * See http://lists.linuxtogo.org/pipermail/openembedded-devel/2009-July/012754.html for more.

recipes/libtool/libtool_2.2.4.bb
@@ -17,10 +17,10 @@ EXTRA_AUTORECONF = "--exclude=libtoolize"
 do_configure_prepend () {
 	# Skip this for native build:
 	if test -n "$CONFIG_SITE" ; then
-		export ac_cv_path_SED=${ac_cv_path_GREP=/bin/sed}
-		export ac_cv_path_GREP=${ac_cv_path_GREP=/bin/grep}
-		export ac_cv_path_EGREP=${ac_cv_path_EGREP=/bin/grep -E}
-		export ac_cv_path_FGREP=${ac_cv_path_FGREP=/bin/grep -F}
+		export ac_cv_path_SED="${ac_cv_path_GREP=/bin/sed}"
+		export ac_cv_path_GREP="${ac_cv_path_GREP=/bin/grep}"
+		export ac_cv_path_EGREP="${ac_cv_path_EGREP=/bin/grep -E}"
+		export ac_cv_path_FGREP="${ac_cv_path_FGREP=/bin/grep -F}"
 	fi
 }
 

recipes/libtool/libtool_2.2.6a.bb
@@ -17,10 +17,10 @@ EXTRA_AUTORECONF = "--exclude=libtoolize"
 do_configure_prepend () {
 	# Skip this for native build:
 	if test -n "$CONFIG_SITE" ; then
-		export ac_cv_path_SED=${ac_cv_path_GREP=/bin/sed}
-		export ac_cv_path_GREP=${ac_cv_path_GREP=/bin/grep}
-		export ac_cv_path_EGREP=${ac_cv_path_EGREP=/bin/grep -E}
-		export ac_cv_path_FGREP=${ac_cv_path_FGREP=/bin/grep -F}
+		export ac_cv_path_SED="${ac_cv_path_GREP=/bin/sed}"
+		export ac_cv_path_GREP="${ac_cv_path_GREP=/bin/grep}"
+		export ac_cv_path_EGREP="${ac_cv_path_EGREP=/bin/grep -E}"
+		export ac_cv_path_FGREP="${ac_cv_path_FGREP=/bin/grep -F}"
 	fi
 }
 