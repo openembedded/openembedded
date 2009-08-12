openembedded/openembedded

commit 013c006d11a59269b9d3adc6cbb6fb8091144965

rdesktop (1.6.0): fix audio redirection for Windows 2008 Server/Vista
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

recipes/rdesktop/rdesktop_1.6.0.bb
@@ -2,6 +2,10 @@ require rdesktop.inc
 
 PR = "${INC_PR}.1"
 
+SRC_URI += " file://audio-2008.patch;patch=1"
+
+SRC_URI += " file://audio-2008.patch;patch=1"
+
 inherit autotools
 
 EXTRA_OECONF = "--with-openssl=${STAGING_EXECPREFIXDIR} "

recipes/rdesktop/rdesktop-1.6.0/audio-2008.patch
@@ -1,0 +1,22 @@
+--- a/rdpdr.c	(revisão 1505)
++++ b/rdpdr.c	(cópia de trabalho)
+@@ -841,14 +841,11 @@
+ RD_BOOL
+ rdpdr_init()
+ {
+-	if (g_num_devices > 0)
+-	{
+-		rdpdr_channel =
+-			channel_register("rdpdr",
+-					 CHANNEL_OPTION_INITIALIZED | CHANNEL_OPTION_COMPRESS_RDP,
+-					 rdpdr_process);
+-	}
+-
++	rdpdr_channel =
++		channel_register("rdpdr",
++				 CHANNEL_OPTION_INITIALIZED | CHANNEL_OPTION_COMPRESS_RDP,
++				 rdpdr_process);
++	
+ 	return (rdpdr_channel != NULL);
+ }
+ 

commit c6dd2a039ee1e28fb62738a2b28a6475be57fee6

rdesktop-1.6.0: Fix build issue
Remove duplicate audio-2008.patch patch line from recipe (fixes build issue
introduced with 013c006d11a59269b9d3adc6cbb6fb8091144965).

Signed-off-by: David-John Willis <John.Willis@Distant-earth.com>
Signed-off-by: Holger Freyther <zecke@selfish.org>

recipes/rdesktop/rdesktop_1.6.0.bb
@@ -5,8 +5,6 @@ PR = "${INC_PR}.2"
 SRC_URI += " file://audio-2008.patch;patch=1"
 SRC_URI_append_ossystems = " file://rdesktop-addin.patch;patch=1"
 
-SRC_URI += " file://audio-2008.patch;patch=1"
-
 inherit autotools
 
 EXTRA_OECONF = "--with-openssl=${STAGING_EXECPREFIXDIR} "