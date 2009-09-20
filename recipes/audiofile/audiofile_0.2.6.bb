openembedded/openembedded

commit bdd37476c0a55d39013e3eb615900606daa2c1e2

audiofile: Security fix CVE-2008-5824, coding style fixes from openSUSE, recipe simplified.

recipes/audiofile/audiofile_0.2.6.bb
@@ -5,19 +5,13 @@ Research, Amiga IFF/8SVX, and NIST SPHERE."
 SECTION = "libs"
 LICENSE = "LGPL GPL"
 RPROVIDES_${PN} += "audiofile"
-PR = "r6"
+PR = "r7"
 
-SRC_URI = "http://www.68k.org/~michael/audiofile/audiofile-${PV}.tar.gz"
+SRC_URI = "http://www.68k.org/~michael/audiofile/audiofile-${PV}.tar.gz \
+	   file://audiofile-m4_quote_fix.diff;patch=1;pnum=0 \
+	   file://audiofile-oldstyle.patch;patch=1;pnum=0 \
+	   file://audiofile-0.2.6.patch;patch=1;pnum=0 \
+	   file://CVE-2008-5824.patch;patch=1 \
+"
 
-inherit autotools pkgconfig binconfig
-
-do_stage () {
-	install -m 0644 libaudiofile/audiofile.h libaudiofile/aupvlist.h libaudiofile/af_vfs.h ${STAGING_INCDIR}/
-	oe_libinstall -C libaudiofile libaudiofile ${STAGING_LIBDIR}
-	install -m 0644 audiofile.m4 ${STAGING_DATADIR}/aclocal/
-}
-
-PACKAGES += "${PN}-bin"
-FILES_${PN} = "${libdir}/libaudiofile*.so.*"
-FILES_${PN}-dev += "${bindir}/*-config"
-FILES_${PN}-bin += "${bindir}"
+inherit autotools lib_package

recipes/audiofile/audiofile-0.2.6/audiofile-0.2.6.patch
@@ -1,0 +1,67 @@
+--- test/instparamtest.c
++++ test/instparamtest.c
+@@ -74,4 +74,5 @@
+ 	printf("release loop id: %ld\n", result);
+ 
+ 	afCloseFile(file);
++	return 0;
+ }
+--- test/instparamwrite.c
++++ test/instparamwrite.c
+@@ -52,4 +52,5 @@
+ 	afSetInstParamLong(file, AF_DEFAULT_INST, AF_INST_NUMCENTS_DETUNE, -30);
+ 
+ 	afCloseFile(file);
++	return 0;
+ }
+--- test/loop.c
++++ test/loop.c
+@@ -34,4 +34,5 @@
+ 	afSetLoopEnd(file, AF_DEFAULT_INST, 2, 4);
+ 
+ 	afCloseFile(file);
++	return 0;
+ }
+--- test/twentyfour.c.xx	2005-09-16 22:38:59.000000000 +0200
++++ test/twentyfour.c	2005-09-16 22:39:53.000000000 +0200
+@@ -36,7 +36,9 @@
+ #endif
+ 
+ #include <stdio.h>
++#include <string.h>
+ #include <stdlib.h>
++#include <unistd.h>
+ #include <audiofile.h>
+ #include <assert.h>
+ 
+--- test/pipe.c.xx	2005-09-16 22:39:36.000000000 +0200
++++ test/pipe.c	2005-09-16 22:39:42.000000000 +0200
+@@ -36,6 +36,7 @@
+ 
+ #include <audiofile.h>
+ #include <stdio.h>
++#include <string.h>
+ #include <stdlib.h>
+ 
+ #define SAMPLE_COUNT 12
+--- test/error.c.xx	2005-09-16 22:40:02.000000000 +0200
++++ test/error.c	2005-09-16 22:40:11.000000000 +0200
+@@ -25,6 +25,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <audiofile.h>
+ 
+ int expectedError;
+--- test/testmarkers.c.xx	2005-09-16 22:40:21.000000000 +0200
++++ test/testmarkers.c	2005-09-16 22:40:41.000000000 +0200
+@@ -25,6 +25,8 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
++#include <unistd.h>
+ #include <assert.h>
+ #include <audiofile.h>
+ 

recipes/audiofile/audiofile-0.2.6/audiofile-m4_quote_fix.diff
@@ -1,0 +1,11 @@
+--- audiofile.m4
++++ audiofile.m4
+@@ -9,7 +9,7 @@
+ dnl AM_PATH_AUDIOFILE([MINIMUM-VERSION, [ACTION-IF-FOUND [, ACTION-IF-NOT-FOUND]]])
+ dnl Test for Audio File Library, and define AUDIOFILE_CFLAGS and AUDIOFILE_LIBS.
+ dnl
+-AC_DEFUN(AM_PATH_AUDIOFILE,
++AC_DEFUN([AM_PATH_AUDIOFILE],
+ [dnl 
+ dnl Get compiler flags and libraries from the audiofile-config script.
+ dnl

recipes/audiofile/audiofile-0.2.6/audiofile-oldstyle.patch
@@ -1,0 +1,55 @@
+--- libaudiofile/g711.c
++++ libaudiofile/g711.c
+@@ -81,10 +81,7 @@
+ 			    0xFFF, 0x1FFF, 0x3FFF, 0x7FFF};
+ 
+ static int
+-search(val, table, size)
+-	int		val;
+-	short		*table;
+-	int		size;
++search(int val, short int *table, int size)
+ {
+ 	int		i;
+ 
+@@ -115,8 +112,8 @@
+  * John Wiley & Sons, pps 98-111 and 472-476.
+  */
+ unsigned char
+-_af_linear2alaw(pcm_val)
+-	int		pcm_val;	/* 2's complement (16-bit range) */
++_af_linear2alaw(int pcm_val)
++                                       /* 2's complement (16-bit range) */
+ {
+ 	int		mask;
+ 	int		seg;
+@@ -151,8 +148,7 @@
+  *
+  */
+ int
+-_af_alaw2linear(a_val)
+-	unsigned char	a_val;
++_af_alaw2linear(unsigned char a_val)
+ {
+ 	int		t;
+ 	int		seg;
+@@ -269,8 +265,7 @@
+ 
+ /* A-law to u-law conversion */
+ static unsigned char
+-alaw2ulaw(aval)
+-	unsigned char	aval;
++alaw2ulaw(unsigned char aval)
+ {
+ 	aval &= 0xff;
+ 	return ((aval & 0x80) ? (0xFF ^ _a2u[aval ^ 0xD5]) :
+@@ -279,8 +274,7 @@
+ 
+ /* u-law to A-law conversion */
+ static unsigned char
+-ulaw2alaw(uval)
+-	unsigned char	uval;
++ulaw2alaw(unsigned char uval)
+ {
+ 	uval &= 0xff;
+ 	return ((uval & 0x80) ? (0xD5 ^ (_u2a[0xFF ^ uval] - 1)) :

commit 69553a3f9961817d02b14ccd27e0e26f3ddfdb5e

audiofile: Fixed staging broken by bdd3747.

recipes/audiofile/audiofile_0.2.6.bb
@@ -5,7 +5,7 @@ Research, Amiga IFF/8SVX, and NIST SPHERE."
 SECTION = "libs"
 LICENSE = "LGPL GPL"
 RPROVIDES_${PN} += "audiofile"
-PR = "r7"
+PR = "r8"
 
 SRC_URI = "http://www.68k.org/~michael/audiofile/audiofile-${PV}.tar.gz \
 	   file://audiofile-m4_quote_fix.diff;patch=1;pnum=0 \
@@ -14,4 +14,4 @@ SRC_URI = "http://www.68k.org/~michael/audiofile/audiofile-${PV}.tar.gz \
 	   file://CVE-2008-5824.patch;patch=1 \
 "
 
-inherit autotools lib_package
+inherit autotools_stage lib_package binconfig