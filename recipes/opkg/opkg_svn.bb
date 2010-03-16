openembedded/openembedded

commit 8c1731d818c89a291f80940c1912587ff9ce46b7

opkg_svn.bb: added missing dependency to openssl to recipe
 * opkg_svn can only be built if openssl is staged, this revision adds the missing dependency

recipes/opkg/opkg_svn.bb
@@ -1,5 +1,7 @@
 require opkg.inc
 
+DEPENDS_append = " openssl"
+
 PR = "${INC_PR}"
 
 PACKAGES =+ "libopkg-dev libopkg"

commit a354bababcb3df9935ebe652a2df353b7205ad9f

opkg: fix global depends on openssl, even in -nogpg/-nocurl
Fixes the problem, introduced by 8c1731d818c89a291f80940c1912587ff9ce46b7,
where openssl is added with DEPENDS_append override, preventing -nogpg and
-nocurl variants from clearing the DEPENDS, when they include opkg_svn.bb
file, thus forcing them to depend on openssl too.

Signed-off-by: Denys Dmytriyenko <denis@denix.org>
Acked-by: Chris Larson <clarson@kergoth.com>

recipes/opkg/opkg.inc
@@ -2,9 +2,9 @@ DESCRIPTION = "Opkg Package Manager"
 DESCRIPTION_libopkg = "Opkg Package Manager Library"
 SECTION = "base"
 LICENSE = "GPLv2"
-DEPENDS = "curl gpgme"
+DEPENDS = "curl gpgme openssl"
 PV = "0.1.6+svnr${SRCPV}"
-INC_PR = "r20"
+INC_PR = "r21"
 
 FILESPATHPKG =. "opkg:"
 

recipes/opkg/opkg_svn.bb
@@ -1,7 +1,5 @@
 require opkg.inc
 
-DEPENDS_append = " openssl"
-
 PR = "${INC_PR}"
 
 PROVIDES =+ "virtual/update-alternatives"