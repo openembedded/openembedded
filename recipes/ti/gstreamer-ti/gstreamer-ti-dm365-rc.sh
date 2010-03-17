977ca9920cafb73c20ee7f4ffcfa7d499f5bcbdc
Denys Dmytriyenko <denis@denix.org>
2010-03-17 15:32:02 -0400

    gstreamer-ti: update dm365 bootscript with the correct values
    Signed-off-by: Denys Dmytriyenko <denis@denix.org>
    Signed-off-by: Koen Kooi <k-kooi@ti.com>

--- recipes/ti/gstreamer-ti/gstreamer-ti-dm365-rc.sh
+++ recipes/ti/gstreamer-ti/gstreamer-ti-dm365-rc.sh
@@ -11,28 +11,22 @@ export GST_TI_TIImgdec_DISABLE=1
 export GST_TI_TIImgenc_DISABLE=1
 
 load_module() {
-    # CMEM Allocation
-    #    1x3628800 Circular buffer
-    #    4x829440  Video buffers (max D1 PAL)
-    #    1x829440  Underlying software components (codecs, etc.)
-    #    1x518400  Underlying software components (codecs, etc.)
-    #    1x4948    Underlying software components (codecs, etc.)
-    #    1x1505280 Underlying software components (codecs, etc.)
-    #    1x5760    Underlying software components (codecs, etc.)
-    #    1x8192    Underlying software components (codecs, etc.)
-    #    1x1       Dummy buffer used during final flush
-    modprobe cmemk phys_start=0x87400000 phys_end=0x88000000 \
-        pools=1x3628800,5x829440,1x518400,1x4948,1x1505280,1x5760,1x8192,1x1
+    # insert cmem
+    modprobe cmemk phys_start=0x85400000 phys_end=0x88000000 \
+        pools=1x6651904,1x3670016,18x1548288,1x282624,1x159744,1x49152,1x32768,1x28672,1x16384,3x12288,2x8192,36x4096
 
-    modprobe dm350mmap
-    rm -f /dev/dm350mmap
-    mknod /dev/dm350mmap c `awk "\\$2==\"dm350mmap\" {print \\$1}" /proc/devices` 0
-
+    modprobe irqk
+    modprobe edmak
+    modprobe dm365mmap
+    rm -f /dev/dm365mmap
+    mknod /dev/dm365mmap c `awk "\\$2==\"dm365mmap\" {print \\$1}" /proc/devices` 0
 }
 
 unload_module() {
-   rmmod cmemk
-   rmmod dm350mmap
+   rmmod cmemk 2>/dev/null
+   rmmod irqk 2>/dev/null
+   rmmod edmak 2>/dev/null
+   rmmod dm365mmap 2>/dev/null
 }
 
 case "$1" in
@@ -58,4 +52,3 @@ case "$1" in
              echo "$0 <start/stop/restart>"
              ;;
 esac
-
