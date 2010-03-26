1fada4c444e4eb49c81c8d54e0de4256dc0f7125
Koen Kooi <koen@openembedded.org>
2010-03-26 11:47:17 +0100

    angstrom-uboot-scripts: enable triple buffering for dsplink script, fixes tearing when using SGX

--- recipes/angstrom/angstrom-uboot-scripts/dsplink.cmd
+++ recipes/angstrom/angstrom-uboot-scripts/dsplink.cmd
@@ -1,4 +1,4 @@
-setenv vram '16M'
-setenv dvimode 'hd720 mem=99M@0x80000000 mem=128M@0x88000000 omapfb.vram=0:8M,1:4M,2:4M'
+setenv vram '20M'
+setenv dvimode 'hd720 mem=99M@0x80000000 mem=128M@0x88000000 omapfb.vram=0:12M,1:4M,2:4M'
 run loaduimage
 run mmcboot
