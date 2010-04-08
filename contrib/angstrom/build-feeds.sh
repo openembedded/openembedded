706c27fffa87d04acb7989894a4d1b9af68a3e30
Paul Menzel <paulepanter@users.sourceforge.net>
2010-04-08 23:51:04 +0200

    Ångström feed builder: Add Enna media center application.
    Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>

--- contrib/angstrom/build-feeds.sh
+++ contrib/angstrom/build-feeds.sh
@@ -122,6 +122,7 @@ do
                       ekiga \
                       empathy \ 
                       emtooth \
+                      enna \
                       eog \
                       epdfview \
                       epiphany \
