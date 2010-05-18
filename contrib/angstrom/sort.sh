34f67a426078e01f447e1a51ec8c126a1e9d8699
Koen Kooi <koen@openembedded.org>
2010-05-18 11:47:43 +0200

    angstrom feed sorter: add more machines

--- contrib/angstrom/sort.sh
+++ contrib/angstrom/sort.sh
@@ -43,47 +43,67 @@ do_sort() {
 archdir=$arch
 
 case "$arch" in
+	"486sx")
+			machines="vortex86sx" ;;
 	"armv4")
-			machines="h3600 h3800 collie simpad htcwallaby" ;;
+			machines="collie h3600 h3800 htcwallaby jornada56x jornada7xx shark simpad" ;;
 	"armv4t")
-			machines="micro2440 mini2440 ep93xx h6300 om-gta01 om-gta02 fic-gta01 fic-gta02" ;;
+			machines="acern30 amsdelta at2440evb cs-e9302 ep93xx eteng500 eten-m800 fic-gta01 fic-gta02 gesbc-9302 h1940 h6300 kb9202 ks8695 m8050 micro2440 mini2440 om-gta01 om-gta02 rx1950 rx3000 sarge-at91 simone smdk2440 smdk2443 topas910 ts72xx" ;;
 	"armv5te")
-			machines="imote2 hawkboard da850-omapl138-evm da830-omapl137-evm htcalpine dm6446-evm dm6467-evm dm6467t-evm dm355-evm dm365-evm dm357-evm topas910 sheevaplug openrd-client openrd-base dm355-leopard n2100 dns323 mv2120 kuropro lspro tsx09 ts409 davinci-dvevm davinci-sffsdr neuros-osd neuros-osd2 gumstix-connex gumstix-verdex gumstix e680 a780 a1200 at91sam9263ek rokre6 rokre2 rokr-e2 akita c7x0 h2200 h3900 h4000 h5000 htcapache htctornado htcblueangel htcuniversal hx4700 nslu2le hx2000 ixp4xxle magician netbook-pro nokia770 palmt650 palmt680 palmld palmtx palmtt3 palmz72 qemuarm omap5912osk poodle spitz tosa mx27ads at91sam9g20ek mainstone hipox" ;;
+			machines="a1200 a780 a910 afeb9260 afeb9260-180 akita asus620 asus730 at91cap9adk at91-l9260 at91sam9260ek at91sam9261ek at91sam9263ek at91sam9g10ek at91sam9g20ek at91sam9g45ek at91sam9g45ekes at91sam9rlek aximx50 aximx50v bd-neon c7x0 chumby cm-x270 cm-x300 da830-omapl137-evm da850-omapl138-evm davinci-dvevm davinci-sffsdr devkitidp-pxa255 dm355-evm dm355-leopard dm357-evm dm365-evm dm6446-evm dm6467-evm dm6467t-evm dns323 e680 ea3250 em-x270 er0100 eseries ghi270 gumstix gumstix-connex gumstix-custom-connex gumstix-custom-verdex gumstix-verdex h1910 h2200 h3900 h4000 h5000 hawkboard hipox htcalpine htcapache htcblueangel htchimalaya htcsable htctornado htcuniversal hx2000 hx4700 imote2 ixp4xxle kixrp435 kuropro logicpd-pxa270 looxc550 lspro magician mainstone mh355 mp900c mtx-3 mtx-3a mv2120 mx21ads mx27ads n2100 navman-icn330 netbook-pro neuros-osd neuros-osd2 nhk15 nokia770 nslu2le omap1510inn omap1610h2 omap1710h3 omap5912osk openrd-base openrd-client palmld palmt650 palmt680 palmtc palmtt palmtt3 palmtt5 palmtx palmz31 palmz71 palmz72 poodle qemuarm rokre2 rokr-e2 rokre6 ronetix-pm9261 ronetix-pm9263 sgh-i900 sheevaplug spitz stamp9g20evb topas910 tosa triton ts409 tsx09 tx25 tx27" ;;
 	"armv5teb")
-			machines="ixp4xxbe nslu2be" ;;
+			machines="fsg3be ixp4xxbe nslu2be" ;;
+	"armv6")
+			machines="bug mx31ads mx31moboard nokia800 om-3d7k omap2420h4 omap2430sdp smartq5 smdk6410" ;;
 	"armv6-novfp")
-			machines="htcdiamond htckaiser htcpolaris htcraphael htcvogue" ;;	
-	"armv6")	
-			machines="smdk6410 smartq5 bug mx31ads nokia800 " ;;
-	"armv7")
-			machines="" ;;
+			machines="htcblackstone htcdiamond htcdream htckaiser htcnike htcpolaris htcraphael htctitan htcvogue" ;;
 	"armv7a")
-			machines="archos5 archos5it palmpre cm-t35 igep0020 omap3-touchbook beagleboard omap3evm am3517-evm omap3517-evm dm3730-am3715-evm omap3-pandora omapzoom omapzoom2 omapzoom36x overo" ;;
+			machines="am3517-evm archos5 archos5it beagleboard bug20 cm-t35 dm3730-am3715-evm htcleo igep0020 nokia900 omap3517-evm omap3evm omap3-pandora omap3-touchbook omap4430-sdp omapzoom omapzoom2 omapzoom36x overo palmpre" ;;
 	"avr32")
-			machines="atngw100 at32stk1000" ;;
+			machines="at32stk1000 atngw100" ;;
 	"bfin")
 			archdir="blackfin"
 			machines="adsp-bf537-stamp" ;;
 	"geode")
-			machines="alix" ;;
+			machines="alix geodegx geodelx iei-nanogx-466 xo" ;;
 	"i486")
-			machines="x86" ;;
+			machines="wrap" ;;
 	"i586")
-			machines="epia" ;;
+			machines="d201gly2 epia i586-generic netvisa progear x86 x86-uml" ;;
 	"i686")
-			machines="eee701 qemux86 guinness progear" ;;
+			machines="eee701 i686-generic qemux86 guinness progear ion qemux86 vmware x86-32-nocona x86-prescott" ;;
 	"iwmmxt")
 			machines="" ;;
-	"ppc405")	
-			machines="dht-walnut" ;;
+	"mips")
+			machines="qemumips" ;;
+	"mipsel")
+			machines="ben-nanonote db1200 lsmipsel mtx-1 mtx-2 rb500 stb225 wgt634u wl500g wrt54 xxs1500" ;;
+	"powerpc")
+			machines="gamecube p2020ds" ;;
+	"ppc405")
+			machines="dht-walnut kilauea magicbox xilinx-ml403 xilinx-ml410" ;;
+	"ppc440e")
+			machines="canyonlands sequoia xilinx-ml507" ;;
+	"ppc500")
+			machines="tqm8540" ;;
 	"ppc603e")
-			machines="lsppchd lsppchg efika n1200" ;;
+			machines="efika lite5200 lsppchd lsppchg n1200 qemuppc storcenter" ;;
+	"ppce300c2")
+			machines="mpc8323e-rdb" ;;
 	"ppce300c3")
-			machines="mpc8313e-rdb mpc8315e-rdb" ;;
+			machines="boc01 mpc8313e-rdb mpc8315e-rdb" ;;
 	"ppce500")
 			machines="tqm8540" ;;
+	"ppce500v2")
+			machines="calamari" ;;
+	"ppce600")
+			machines="mpc8641-hpcn" ;;
+	"sh4")
+			machines="qemush4 titan" ;;
 	"sparc")
-			machines="" ;;
+			machines="sun4cdm" ;;
+	"x86")
+			machines="colinux" ;;
 esac
 
 if [ $(find . -name  "*.ipk"| grep $arch | wc -l) -gt 0 ] ; then
