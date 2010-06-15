0c38732598cb6525f1fd487fe966e67355179e7e
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2010-06-15 16:51:37 +0400

    angstrom feed sorter: add 486sx arch and drop armv7 from the arches list
    Add 486sx (vortex86sx machine) and drop armv7 (no such thing exists
    anymore).
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- contrib/angstrom/sort.sh
+++ contrib/angstrom/sort.sh
@@ -183,7 +183,7 @@ for i in `find . -name  "*.ipk"| grep _all` ; do mkdir -p ../all/ || true ;mv $i
 mkdir -p ../sdk ; mv *sdk.ipk ../sdk/ || true
  (mkdir -p ../sdk ; cd ../sdk && ipkg-make-index -p Packages -m . >& /dev/null ; touch Packages.sig )
 
-for arch in armv4t armv4 armv5teb armv5te armv6-novfp armv6 armv7a armv7 avr32 bfin geode i486 i586 i686 iwmmxt mips mipsel powerpc ppc405 ppc440e ppce300c2 ppce300c3 ppce500v2 ppce500 ppce600 sh4 sparc x86_64 x86; do
+for arch in 486sx armv4t armv4 armv5teb armv5te armv6-novfp armv6 armv7a avr32 bfin geode i486 i586 i686 iwmmxt mips mipsel powerpc ppc405 ppc440e ppc603e ppce300c2 ppce300c3 ppce500v2 ppce500 ppce600 sh4 sparc x86_64 x86; do
 	do_sort
 done
 
