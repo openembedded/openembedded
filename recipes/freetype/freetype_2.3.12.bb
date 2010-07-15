openembedded/openembedded

commit aaac36c39e301995029cb40ae40fa8af277694dc

freetype: updated to 2.3.12
That's (nearly) a copy of the old 2.3.11 recipe.

Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>

recipes/freetype/freetype-2.3.12/fix-configure.patch
@@ -1,0 +1,13 @@
+Index: freetype-2.3.6/builds/unix/configure.ac
+===================================================================
+--- freetype-2.3.6.orig/builds/unix/configure.ac
++++ freetype-2.3.6/builds/unix/configure.ac
+@@ -506,8 +506,6 @@ AC_SUBST([FT2_EXTRA_LIBS])
+ AC_SUBST([SYSTEM_ZLIB])
+
+
+-LT_INIT(win32-dll)
+-
+ AC_SUBST([hardcode_libdir_flag_spec])
+ AC_SUBST([wl])
+ AC_SUBST([build_libtool_libs])

recipes/freetype/freetype-2.3.12/libtool-tag.patch
@@ -1,0 +1,20 @@
+--- freetype-2.3.6/builds/unix/unix-cc.in
++++ freetype-2.3.6/builds/unix/unix-cc.in
+@@ -88,7 +88,7 @@
+ #
+ #
+ CCraw := $(CC)
+-CC    := $(LIBTOOL) --mode=compile $(CCraw)
++CC    := $(LIBTOOL) --tag=CC --mode=compile $(CCraw)
+
+ # Linker flags.
+ #
+@@ -105,7 +105,7 @@
+
+ # Library linking
+ #
+-LINK_LIBRARY = $(LIBTOOL) --mode=link $(CCraw) -o $@ $(OBJECTS_LIST) \
++LINK_LIBRARY = $(LIBTOOL) --tag=CC --mode=link $(CCraw) -o $@ $(OBJECTS_LIST) \
+                           -rpath $(libdir) -version-info $(version_info) \
+                           $(LDFLAGS) -no-undefined \
+                           # -export-symbols $(EXPORTS_LIST)

recipes/freetype/freetype_2.3.12.bb
@@ -1,0 +1,40 @@
+DESCRIPTION = "Freetype font rendering library"
+SECTION = "libs"
+LICENSE = "freetype"
+PR = "r0"
+
+SRC_URI = "\
+  ${SOURCEFORGE_MIRROR}/freetype/freetype-${PV}.tar.bz2 \
+  file://no-hardcode.patch \
+  file://fix-configure.patch \
+  file://libtool-tag.patch \
+"
+S = "${WORKDIR}/freetype-${PV}"
+
+SRC_URI[md5sum] = "e974a82e5939be8e05ee65f07275d7c5"
+SRC_URI[sha256sum] = "3b96438f016a62b676c1d2089c00ca777f710d19f6aefa66ccf068d360db3e92"
+
+inherit autotools pkgconfig binconfig
+
+LIBTOOL = "${HOST_SYS}-libtool"
+EXTRA_OEMAKE = "'LIBTOOL=${LIBTOOL}'"
+EXTRA_OECONF = "--without-zlib"
+
+do_configure() {
+	cd builds/unix
+	libtoolize --force --copy
+	gnu-configize --force
+	aclocal -I .
+	autoconf
+	cd ${S}
+	oe_runconf
+}
+
+do_compile_prepend() {
+	${BUILD_CC} -o objs/apinames src/tools/apinames.c
+}
+
+BBCLASSEXTEND = "native"
+
+FILES_${PN} = "${libdir}/lib*.so.*"
+FILES_${PN}-dev += "${bindir}"

commit ac70c8e343b668d016d474cd2fcf61784577beaf

freetype: fixed broken patches
Patches in aaac36c39e301995029cb40ae40fa8af277694dc are malformed
because they were cherrypicked with global 'apply.whitespace=fix'.

Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>

recipes/freetype/freetype-2.3.12/fix-configure.patch
@@ -4,8 +4,8 @@ Index: freetype-2.3.6/builds/unix/configure.ac
 +++ freetype-2.3.6/builds/unix/configure.ac
 @@ -506,8 +506,6 @@ AC_SUBST([FT2_EXTRA_LIBS])
  AC_SUBST([SYSTEM_ZLIB])
-
-
+ 
+ 
 -LT_INIT(win32-dll)
 -
  AC_SUBST([hardcode_libdir_flag_spec])

recipes/freetype/freetype-2.3.12/libtool-tag.patch
@@ -1,16 +1,16 @@
---- freetype-2.3.6/builds/unix/unix-cc.in
-+++ freetype-2.3.6/builds/unix/unix-cc.in
-@@ -88,7 +88,7 @@
+--- freetype-2.3.6/builds/unix/unix-cc.in	
++++ freetype-2.3.6/builds/unix/unix-cc.in	
+@@ -88,7 +88,7 @@ 
  #
  #
  CCraw := $(CC)
 -CC    := $(LIBTOOL) --mode=compile $(CCraw)
 +CC    := $(LIBTOOL) --tag=CC --mode=compile $(CCraw)
-
+ 
  # Linker flags.
  #
-@@ -105,7 +105,7 @@
-
+@@ -105,7 +105,7 @@ 
+ 
  # Library linking
  #
 -LINK_LIBRARY = $(LIBTOOL) --mode=link $(CCraw) -o $@ $(OBJECTS_LIST) \

recipes/freetype/freetype_2.3.12.bb
@@ -1,7 +1,7 @@
 DESCRIPTION = "Freetype font rendering library"
 SECTION = "libs"
 LICENSE = "freetype"
-PR = "r1"
+PR = "r2"
 DEPENDS = "zlib"
 
 SRC_URI = "\