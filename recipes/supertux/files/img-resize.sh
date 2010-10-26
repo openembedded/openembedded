a06a0243c093ee79c8151bb70b69cd89f3668ead
Andreas Oberritter <obi@opendreambox.org>
2010-10-26 15:43:30 +0000

    supertux: fix sh equality operator
    Signed-off-by: Andreas Oberritter <obi@opendreambox.org>
    Acked-by: Michael Smith <msmith@cbnco.com>

--- recipes/supertux/files/img-resize.sh
+++ recipes/supertux/files/img-resize.sh
@@ -10,14 +10,14 @@ fi
 for i in org/*; do
 	b=`basename $i`
 	ext=`expr "x$b" : 'x.*\.\(.*\)'`
-	if [ "$ext" == "png" ]; then
-	    if [ $b == "tux-life.png" ]; then
+	if [ "$ext" = "png" ]; then
+	    if [ $b = "tux-life.png" ]; then
 		cp $i _.png
 	    else
 		convert $i -resize 50% -colors 64 _.png
 	    fi
 	    pngcrush -q _.png $b
-	elif [ "$ext" == "jpg" ]; then
+	elif [ "$ext" = "jpg" ]; then
 		convert $i -resize 50% -quality 60 $b
 	fi
 done
