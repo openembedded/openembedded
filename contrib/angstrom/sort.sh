c7a7e48163103812c6fa8413004c0af3bd30ae1a
Chase Maupin <Chase.Maupin@ti.com>
2010-10-28 16:53:52 -0500

    sort.sh: add support for am180x-evm machine type
    * Add support for the am180x-evm machine type to the
      sort.sh script.
    * Break sort.sh line up into multiple lines because it was longer
      than 998 characters and git send-mail would not send the patch.
    
    Signed-off-by: Chase Maupin <Chase.Maupin@ti.com>
    Signed-off-by: Koen Kooi <k-kooi@ti.com>

--- contrib/angstrom/sort.sh
+++ contrib/angstrom/sort.sh
@@ -54,7 +54,24 @@ case "$arch" in
 	"armv4t")
 			machines="acern30 amsdelta at2440evb cs-e9302 ep93xx eteng500 eten-m800 fic-gta01 fic-gta02 gesbc-9302 h1940 h6300 kb9202 ks8695 m8050 micro2440 mini2440 om-gta01 om-gta02 rx1950 rx3000 sarge-at91 simone smdk2440 smdk2443 topas910 ts72xx" ;;
 	"armv5te")
-			machines="a1200 a780 a910 afeb9260 afeb9260-180 akita asus620 asus730 at91cap9adk at91-l9260 at91sam9260ek at91sam9261ek at91sam9263ek at91sam9g10ek at91sam9g20ek at91sam9g45ek at91sam9g45ekes at91sam9rlek aximx50 aximx50v bd-neon c7x0 chumby chumby-falconwing cm-x270 cm-x300 da830-omapl137-evm da850-omapl138-evm davinci-dvevm davinci-sffsdr devkitidp-pxa255 dm355-evm dm355-leopard dm357-evm dm365-evm dm6446-evm dm6467-evm dm6467t-evm dns323 dockstar e680 ea3250 em-x270 er0100 eseries ghi270 gumstix gumstix-connex gumstix-custom-connex gumstix-custom-verdex gumstix-verdex h1910 h2200 h3900 h4000 h5000 hawkboard hipox htcalpine htcapache htcblueangel htchimalaya htcsable htctornado htcuniversal hx2000 hx4700 imote2 ixp4xxle kixrp435 kuropro logicpd-pxa270 looxc550 lspro magician mainstone mh355 mp900c mtx-3 mtx-3a mv2120 mx21ads mx27ads n2100 navman-icn330 netbook-pro neuros-osd neuros-osd2 nhk15 nokia770 nslu2le omap1510inn omap1610h2 omap1710h3 omap5912osk openrd-base openrd-client palmld palmt650 palmt680 palmtc palmtt palmtt3 palmtt5 palmtx palmz31 palmz71 palmz72 poodle qemuarm rokre2 rokr-e2 rokre6 ronetix-pm9261 ronetix-pm9263 sgh-i900 sheevaplug spitz stamp9g20evb topas910 tosa triton ts409 tsx09 tx25 tx27" ;;
+			machines="a1200 a780 a910 afeb9260 afeb9260-180 akita am180x-evm
+ asus620 asus730 at91cap9adk at91-l9260 at91sam9260ek at91sam9261ek
+ at91sam9263ek at91sam9g10ek at91sam9g20ek at91sam9g45ek at91sam9g45ekes
+ at91sam9rlek aximx50 aximx50v bd-neon c7x0 chumby chumby-falconwing cm-x270
+ cm-x300 da830-omapl137-evm da850-omapl138-evm davinci-dvevm davinci-sffsdr
+ devkitidp-pxa255 dm355-evm dm355-leopard dm357-evm dm365-evm dm6446-evm
+ dm6467-evm dm6467t-evm dns323 dockstar e680 ea3250 em-x270 er0100 eseries
+ ghi270 gumstix gumstix-connex gumstix-custom-connex gumstix-custom-verdex
+ gumstix-verdex h1910 h2200 h3900 h4000 h5000 hawkboard hipox htcalpine
+ htcapache htcblueangel htchimalaya htcsable htctornado htcuniversal hx2000
+ hx4700 imote2 ixp4xxle kixrp435 kuropro logicpd-pxa270 looxc550 lspro
+ magician mainstone mh355 mp900c mtx-3 mtx-3a mv2120 mx21ads mx27ads n2100
+ navman-icn330 netbook-pro neuros-osd neuros-osd2 nhk15 nokia770 nslu2le
+ omap1510inn omap1610h2 omap1710h3 omap5912osk openrd-base openrd-client
+ palmld palmt650 palmt680 palmtc palmtt palmtt3 palmtt5 palmtx palmz31
+ palmz71 palmz72 poodle qemuarm rokre2 rokr-e2 rokre6 ronetix-pm9261
+ ronetix-pm9263 sgh-i900 sheevaplug spitz stamp9g20evb topas910 tosa
+ triton ts409 tsx09 tx25 tx27" ;;
 	"armv5teb")
 			machines="fsg3be ixp4xxbe nslu2be" ;;
 	"armv6")
