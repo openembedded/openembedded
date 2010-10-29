291693ea81a4b65fbb7f75db10790d72d37b1ec0
Eric Bénard <eric@eukrea.com>
2010-10-29 17:44:52 +0200

    busybox/find-touchscreen: handle input/device case
    On a new configuration using 2.6.36 kernel + busybox 1.17.1 ,
    find-toucscreen.sh fails because $MDEV is input/event0 and not event0.
    
    This patch fixes this problem by removing input/ if present in $MDEV.
    
    Signed-off-by: Eric Bénard <eric@eukrea.com>

--- recipes/busybox/files/find-touchscreen.sh
+++ recipes/busybox/files/find-touchscreen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+MDEV=`echo $MDEV | sed 's#input/##g'`
+
 if [ `egrep "input:.*-e0.*,3,.*a0,1,.*18,.*" /sys/class/input/$MDEV/device/modalias|wc -l` -gt 0 ]; then
 	ln -sf /dev/input/$MDEV /dev/input/touchscreen0
 fi
