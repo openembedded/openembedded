fd25d654ca692e768d41147d389731a2a5f8dd4e
Eric Bénard <eric@eukrea.com>
2010-10-30 02:35:50 +0200

    add missing script for usb detection
    this script is missing in commit cd7c9542bad478df9df733a6639ab3b6ef41ef72
    
    Signed-off-by: Eric Bénard <eric@eukrea.com>

---
+++ recipes/busybox/files/usb.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+case "$ACTION" in
+  add|"")
+    for uevent in /sys/class/usb_device/usbdev?.*/*/uevent; do
+      . $uevent
+      if [ ! -e /dev/bus/usb/$BUSNUM/$DEVNUM ]; then
+        mkdir -p /dev/bus/usb/$BUSNUM
+        mknod /dev/bus/usb/$BUSNUM/$DEVNUM c 189 $MINOR
+      fi
+    done
+    ;;
+  remove)
+    for device in /dev/bus/usb/*/*; do
+      REMOVED=1
+      dev=`basename $device`
+      bus=`basename $(dirname $device)`
+      for uevent in /sys/class/usb_device/usbdev?.*/*/uevent; do
+        . $uevent
+        echo $dev $DEVNUM $bus $BUSNUM >> /tmp/rem.txt
+        if [ $dev -eq $DEVNUM ] && [ $bus -eq $BUSNUM ]; then
+          REMOVED=0
+          break;
+        fi
+      done
+      if [ $REMOVED -eq 1 ]; then
+        rm /dev/bus/usb/$bus/$dev
+        if [ -z $(ls /dev/bus/usb/$bus/) ]; then
+          rmdir /dev/bus/usb/$bus/
+        fi
+      fi
+    done
+    ;;
+esac
+-- 
+1.6.3.3
+
