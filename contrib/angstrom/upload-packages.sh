04c25d50ce2f6d547349701085bb8ac2c3eb3a72
Koen Kooi <koen@openembedded.org>
2011-03-04 14:47:46 +0100

    angstrom feed scripts: Upload to the EU mirror by default
    based on 2ac51f0adb64e5001d8e3f1d31d399b3dbaef3b5 from org.openembedded.dev
    
    Signed-off-by: Koen Kooi <koen@openembedded.org>

--- contrib/angstrom/upload-packages.sh
+++ contrib/angstrom/upload-packages.sh
@@ -5,7 +5,7 @@
 # Run this from inside ${DEPLOY_DIR} e.g. tmp/deploy/glibc/
 
 # Angstrom webserver
-REMOTEM=angstrom@feeds.angstrom-distribution.org
+REMOTEM=angstrom@eu.feeds.angstrom-distribution.org
 
 # Feed dir we want to upload to
 REMOTED=website/feeds/unstable/ipk/$(basename $PWD)
