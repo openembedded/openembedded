shr-distribution/meta-smartphone

xserver-xorg-conf: add support for newer kernel for the nokia900
Note that there are whitespaces issues in the nokia900's xorg.conf
Also note that it may not be the final xorg.conf since the kernel
  defconfig may change to add new input devices(sensors etc...)

Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

recipes-graphics/xorg-xserver/xserver-xorg-conf_0.1.bb
@@ -1,5 +1,5 @@
 DESCRIPTION = "Machine specific xorg.conf files"
-PR = "r42"
+PR = "r43"
 LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${TOPDIR}/meta-shr/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
 

openembedded/openembedded

recipes/xorg-xserver/xserver-xorg-conf_0.1.bb
@@ -1,5 +1,5 @@
 DESCRIPTION = "Machine specific xorg.conf files"
-PR = "r45"
+PR = "r46"
 
 SRC_URI = "file://xorg.conf"
 